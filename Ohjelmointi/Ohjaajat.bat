ec9ab939606f29c3efab1e6d2c034da077a0b0bd
jlpturkk <jyri.turkki@gmail.com>
2022-06-03 18:13:01 +0300

    tietokannat eka versio

---
+++ Ohjelmointi/Ohjaajat.bat
@@ -0,0 +1,6 @@
+Ohjaajat:
+;Kenttien järjestys tiedostossa:
+; id |Sukunimi Etunimi   | Erikoistaito
+  1  |Tuuba Tontsa       | Purkanjauhaminen
+  2  |Jurtta Jaakko      | Irvistely
+  3  |Orava Kurre        | Kansainvalinen kuikuilu
---
+++ Ohjelmointi/workshopit.bat
@@ -0,0 +1,7 @@
+Workshopit
+;Kenttien järjestys tiedostossa:
+; id | workshop   | Sijainti   | max_os | vetaja
+ 1   |Ohjelmointi | Teltta     | 6      | 1
+ 2   |Testaus     | Kanervikko | 2      | 2
+ 3   |Levitointi  | Kellari    | 5      | 3
+ 4   |Ihmettely   | Piha       | 10     | 1
