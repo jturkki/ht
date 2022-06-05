0ba6a9b053d03b2930a12777d11f2d612fb697a7
jlpturkk <jyri.turkki@gmail.com>
2022-06-05 13:25:10 +0300

    lisää kuvia

---
+++ Ohjelmointi/osallistuu_workshoppiin.bat
@@ -0,0 +1,9 @@
+osallistuu_workshoppiin
+;Kenttien järjestys tiedostossa:
+; osallistuja id | workshop id
+        1        |    1
+        1        |    2
+        1        |    3
+        2        |    2
+        2        |    4
+        3        |    3
